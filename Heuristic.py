1575c8257bb53873648b89843b37bfdf632271e0
xaedes <xaedes@gmail.com>
2014-11-12 19:49:21 +0100

    added traveled distance and heading to heuristic

--- Heuristic.py
+++ Heuristic.py
@@ -1,21 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import division    # Standardmäßig float division - Ganzzahldivision kann man explizit mit '//' durchführen
-import numpy as np                 # Numpy, das Number One Number Crunching Tool
+from __future__ import \
+    division  # Standardmäßig float division - Ganzzahldivision kann man explizit mit '//' durchführen
+import numpy as np  # Numpy, das Number One Number Crunching Tool
 import math
+
+
 class Heuristic(object):
-	"""docstring for Heuristic"""
-	def __init__(self, lane):
+    """docstring for Heuristic"""
 
-		super(Heuristic, self).__init__()
-		self.lane = lane
+    def __init__(self, lane):
+        super(Heuristic, self).__init__()
+        self.lane = lane
+        self.traveled = None
 
-	def evaluate(self, car):
-		score = 0
-		closest = self.lane.closest_sampled_idx(car.x,car.y)
-		closest = np.array(self.lane.sampled_x[closest], self.lane.sampled_y[closest])
-		distance = math.sqrt(np.sum(np.square(closest-np.array([car.x,car.y]))))
+    def evaluate(self, car):
+        score = 0
+        closest_idx = self.lane.closest_sampled_idx(car.x, car.y)
 
-		score -= distance
+        # initialize odometry
+        if self.traveled is None:
+            self.traveled = closest_idx
 
-		return score
+        # update odometry
+        diff = closest_idx - self.traveled
+        diff -= np.round(diff / self.lane.path_len) * self.lane.path_len
+        self.traveled += diff
+        score += self.traveled 
+        # print self.traveled 
+
+        closest = np.array(self.lane.sampled_x[closest_idx], self.lane.sampled_y[closest_idx])
+        distance = math.sqrt(np.sum(np.square(closest - np.array([car.x, car.y]))))
+        # print distance
+        score -= distance
+
+        tangents = self.lane.sample_tangents()
+        # print tangents 
+        print car.theta , tangents[closest_idx]
+        diff = car.theta - tangents[closest_idx]
+        diff -= np.round(diff / 360) * 360
+        print diff
+        score -= abs(diff)* 10
+        # print tangents[closest_idx] 
+
+        # pri
+
+        return score
