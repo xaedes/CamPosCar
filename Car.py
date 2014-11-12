1d79b922df11290aa95fbd3153ff38024a040683
xaedes <xaedes@gmail.com>
2014-11-12 18:44:38 +0100

    made car dynamics more realistic and allow 5 different steers instead of 3

--- Car.py
+++ Car.py
@@ -7,20 +7,20 @@ d2r = math.pi / 180
 
 class Car(object):
     """docstring for Car"""
-    def __init__(self, x, y, theta, speed=0.1, max_steer=0.2, size=10):
+    def __init__(self, x, y, theta, speed=0.03, max_steer=0.3, size=10):
         super(Car, self).__init__()
         self.x = x
         self.y = y
         self.theta = theta # in degree
         self.size = size
         self.speed = speed # px / s
-        self.actions = np.array([-1,0,1])
+        self.actions = np.linspace(-1,1,5)
         self.max_steer = max_steer # in degree/s
     def steers(self):
         return self.actions * self.max_steer
 
     def forward(self,action,dt):
-        assert action in [-1,0,1]
+        assert action in self.actions
     #     # action == -1  : left
     #     # action ==  0  : middle
     #     # action ==  1  : right
