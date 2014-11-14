8d36df46c971101d7a34b2f32265b8a2595216fb
xaedes <xaedes@gmail.com>
2014-11-14 18:14:38 +0100

    removed error catchers just throw error, as this cases should not happen at all

--- Node.py
+++ Node.py
@@ -11,11 +11,7 @@ class Node(object):
         self.action = action
         self.prevNode = prevNode
         self.children = []
-        if Node.heuristic is not None:
-            self.heuristic_value = Node.heuristic.evaluate(self)
-        else:
-            self.heuristic_value = None
-
+        self.heuristic_value = Node.heuristic.evaluate(self)
         self.best_heuristic_value_from_here = self.heuristic_value
 
         if self.prevNode is not None:
@@ -23,13 +19,12 @@ class Node(object):
             self.prevNode.children.append(self)
 
             # propagate heuristic value to parents
-            if self.heuristic_value is not None:
-                current = self
-                while current.prevNode is not None:
-                    current.prevNode.best_heuristic_value_from_here = max(
-                        current.prevNode.best_heuristic_value_from_here,
-                        current.best_heuristic_value_from_here)
-                    current = current.prevNode
+            current = self
+            while current.prevNode is not None:
+                current.prevNode.best_heuristic_value_from_here = max(
+                    current.prevNode.best_heuristic_value_from_here,
+                    current.best_heuristic_value_from_here)
+                current = current.prevNode
 
         else:
             self.depth = 0
