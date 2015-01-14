633cb8842f7a657a22a861828b419f4d9b0f532f
xaedes <xaedes@gmail.com>
2015-01-14 19:18:46 +0100

    subimage now kinda works

--- CamView.py
+++ CamView.py
@@ -10,14 +10,15 @@ import cv2
 
 from cv2 import cv
 
-
+from Draw import Draw
+from Utils import Utils
 
 class CamView(object):
     """docstring for ClassName"""
-    def __init__(self, car, imageArr,width = 200,height = 200):
+    def __init__(self, car, imageArr,width = 200,height = 150):
         super(CamView, self).__init__()
         self.car = car
-        self.img = imageArr
+        self.img = imageArr.transpose(1, 0, 2) # flip x and y axes
         self.width = width
         self.height = height
 
@@ -29,16 +30,22 @@ class CamView(object):
 
     def draw(self, screen):
         # pos = (self.height-self.car.y, self.width-self.car.x)
-        pos = (self.xy[0],self.xy[1])
-        print pos
-        theta = 0
-        # theta = self.car.theta
-        view = self.subimage(self.img, pos, 1*(3.14/2)+theta * 3.14 / 180, self.width, self.height)
+        pos = (self.car.x, self.car.y)
+        # pos = (self.xy[0],self.xy[1])
+        # print pos,
+        # print self.img.shape,
+        # print self.img.transpose(1, 0, 2).shape,
+        # print ""
+        # theta = 22.5
+        theta = self.car.theta
+        # cv2.imshow("foo?",self.img)
+        view = self.subimage(self.img, pos, 0*90+theta , self.width, self.height)
         cv2.imshow("",view)
         cv2.waitKey(1)
         # surf = pygame.surfarray.make_surface(view)
 
         # screen.blit(surf,(0,0))
+        Draw.draw_rotated_rect(screen,pos[0],pos[1],self.width,self.height,theta,Draw.RED)
         # pass
 
     def register_events(self,events):
@@ -59,12 +66,34 @@ class CamView(object):
             self.xy = event.pos
             
 
-    def subimage(self,image, centre, theta, width, height):
-        # http://stackoverflow.com/a/11627903/798588
-       # output_image = cv.CreateImage((width, height), image.depth, image.nChannels)
-       mapping = np.array([[np.cos(theta), -np.sin(theta), centre[0]],
-                           [np.sin(theta), np.cos(theta), centre[1]]])
-       # map_matrix_cv = cv.fromarray(mapping)
-       # cv.GetQuadrangleSubPix(image, output_image, map_matrix_cv)
-       return cv2.warpAffine(image,mapping,(width, height),borderMode=cv2.BORDER_REPLICATE)
-       # return output_image    
+    # def subimage(self,image, centre, theta, width, height):
+    #     # http://stackoverflow.com/a/11627903/798588
+    #     # output_image = cv.CreateImage((width, height), image.depth, image.nChannels)
+    #     mapping = np.array([[np.cos(theta), -np.sin(theta), -centre[0]],
+    #                        [np.sin(theta), np.cos(theta), -centre[1]]])
+    #     print mapping
+    #     # map_matrix_cv = cv.fromarray(mapping)
+    #     # cv.GetQuadrangleSubPix(image, output_image, map_matrix_cv)
+    #     return cv2.warpAffine(image,mapping,(width, height),borderMode=cv2.BORDER_REPLICATE)
+    #     # return output_image    
+
+    # theta in degree
+    def subimage(self,image, center, theta, width, height):
+
+        #http://answers.opencv.org/question/497/extract-a-rotatedrect-area/
+        # thanks to http://felix.abecassis.me/2011/10/opencv-rotation-deskewing/
+        if theta < -45.:
+            theta += 90.0
+            height, width = width, height
+        
+        # get the rotation matrix
+        M = cv2.getRotationMatrix2D(center, theta , 1.0) 
+        # perform the affine transformation
+        warped = cv2.warpAffine(image,M,image.shape[:2],borderMode=cv2.BORDER_REPLICATE,flags=cv2.INTER_NEAREST)
+
+        # return warped
+        # crop the resulting image
+        cropped = cv2.getRectSubPix(warped, (width, height), center)
+        # croppes = warped[round(center-width/2)]
+
+        return cropped
